2866586258a891f99c140f0caebfb3dab2acf7c6
Bhargav <bhargav.dev01@gmail.com>
2025-01-25 22:01:07 +0530

    interview sesssions add

--- _Interview_/Techincal/asked_programs.py
+++ _Interview_/Techincal/asked_programs.py
@@ -208,3 +208,86 @@ print( longest(str_value))
 
 
 
+
+
+def maxConsecutiveZero(nums, k):
+    left = 0
+    max_length = 0
+    zero_count = 0
+    
+    for right in range(len(nums)):
+        
+        # count zeros
+        if nums[right] == 0:
+            zero_count +=1
+        
+        while zero_count > k:
+            if nums[left] == 0:
+                zero_count -=1
+            left +=1
+    max_length = max(max_length, right - left+1)
+    
+    return max_length
+
+nums,k = [1,1,1,0,0,0,1,1,1,1,0], 2
+
+print(maxConsecutiveZero(nums, k))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import re
+
+# str_var = "sadnlwjrkwejk42kiraaas472hajkhsaqqoo978"
+
+
+# pattern_output = re.findall(r"\b\d{3}\b", str_var)
+
+# output = [int(num) for num in pattern_output]
+# print(output)
